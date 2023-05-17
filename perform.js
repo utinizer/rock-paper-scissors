a499ce8ee1dee104d159c91a43f4f7466d9e07dd
utinizer <ajmorga0908@gmail.com>
2023-05-17 03:30:45 -0400

    Completed the match function

--- perform.js
+++ perform.js
@@ -11,7 +11,7 @@ function getComputerChoice(){
 }
 
 // Prompting the user to choose rock, paper, or scissors
-const playerChoice = prompt("Rock, paper, or scissors?");
+const playerChoice = "rock";
 
 // Capitalize the users choice properly to make it more uniform (optional)
 function upperCase(playerSelection){
@@ -31,7 +31,7 @@ function playRound(playerSelection,computerSelection){
     if ((playerSelection === "Rock" && computerSelection === "Rock") ||
     (playerSelection === "Paper" && computerSelection === "Paper") ||
     (playerSelection === "Scissors" && computerSelection === "Scissors")){
-        return `${playerSelection} against ${computerSelection} means a draw!`;
+        return `It's a draw! ${playerSelection} against ${computerSelection} is a tie`;
     } else if ((playerSelection === "Rock" && computerSelection === "Paper") ||
     (playerSelection === "Paper" && computerSelection === "Scissors") ||
     (playerSelection === "Scissors" && computerSelection === "Rock")) {
@@ -39,10 +39,48 @@ function playRound(playerSelection,computerSelection){
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
     (playerSelection === "Paper" && computerSelection === "Rock") ||
     (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
         return "You must input a valid option";
     }
 }
 
-console.log(playRound(playerSelection,computerSelection));
+        // Record initial scores of player and computer
+        let playerScore = 0;
+        let compScore = 0;
+
+// Creating a function to play a 5 round game
+function game(){
+    for (let i = 0; i < 5; i++){
+        // Getting player choice and changing its casing properly
+        const playerChoice = prompt("Choose rock, paper, or scissors");
+        const playerSelection = upperCase(playerChoice);
+        // Getting computer choice
+        const computerSelection = getComputerChoice();
+        // Play a round
+        let result = playRound(playerSelection,computerSelection);
+        // Add score to appropriate player
+        if (result === `It's a draw! ${playerSelection} against ${computerSelection} is a tie`){
+            playerScore = playerScore;
+        } else if (result === `You Lose! ${computerSelection} beats ${playerSelection}`){
+            compScore++;
+        } else if (result === `You Win! ${playerSelection} beats ${computerSelection}`){
+            playerScore++;
+        } else {
+            i--;
+        }
+        // Announce result of round
+        console.log(`${result}\nThe score is ${playerScore} - ${compScore}\n`);
+    }
+    // Return the final score and the winner
+    if (playerScore > compScore){
+        return `The final score is ${playerScore} - ${compScore}\nYou win!`;
+    } else if (playerScore < compScore){
+        return `The final score is ${playerScore} - ${compScore}\nYou lose!`;
+    } else {
+        return `The final score is ${playerScore} - ${compScore}\nIt's a draw!`;
+    }
+}
+
+// Log the match
+console.log(game());
